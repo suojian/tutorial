f00fe08cadbd8dcd569b217e4a5c957b90760d98
jsuo <suo_jian@hotmail.com>
2022-09-12 15:36:47 -0700

    create message system schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,19 @@
 CREATE SCHEMA `tutorial` ;
+
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `read_status` tinyint(4) DEFAULT 0,
+  `content` varchar(500) NOT NULL,
+  `sent_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `block_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blocker_id` int(11) NOT NULL,
+  `blocked_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
