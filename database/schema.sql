cd3da5dd7fef662e1d94d03236d2888e9bc1138b
jsuo <suo_jian@hotmail.com>
2022-09-15 17:51:12 -0700

    create student tutorial system database schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,5 +1,45 @@
 CREATE SCHEMA `tutorial` ;
 
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `grade` int(11) NOT NULL,
+  `display_name` varchar(20) NOT NULL,
+  `full_name` varchar(30) NOT NULL,
+  `school` varchar(100) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `intro` varchar(500) DEFAULT NULL,
+  `interest_tags` varchar(500) DEFAULT NULL,
+  `register_date` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `display_name_UNIQUE` (`display_name`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `course_info` varchar(2000) DEFAULT NULL,
+  `video` varchar(200) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `course_tags` varchar(200) DEFAULT NULL,
+  `course_status` tinyint(4) NOT NULL DEFAULT 1,
+  `unit_price` decimal(4,2) DEFAULT 0.00,
+  `picture1` longtext DEFAULT NULL,
+  `picture2` longtext DEFAULT NULL,
+  `picture3` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `course_subscribe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `is_archived` tinyint(4) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sender_id` int(11) NOT NULL,
