9c6303e8f3715b7c756df394f20b6711e539cb2f
jsuo <suo_jian@hotmail.com>
2022-09-26 20:01:57 -0700

    update foreign key constraints

--- database/schema.sql
+++ database/schema.sql
@@ -28,15 +28,22 @@ CREATE TABLE `course` (
   `picture1` longtext DEFAULT NULL,
   `picture2` longtext DEFAULT NULL,
   `picture3` longtext DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_course_users_idx` (`user_id`),
+  CONSTRAINT `fk_course_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE `course_subscribe` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
   `course_id` int(11) DEFAULT NULL,
   `is_archived` tinyint(4) DEFAULT 0,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_c_u_idx` (`course_id`),
+  KEY `fk_s_u_idx` (`user_id`),
+  CONSTRAINT `fk_c_u` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_s_u` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -47,15 +54,24 @@ CREATE TABLE `messages` (
   `read_status` tinyint(4) DEFAULT 0,
   `content` varchar(500) NOT NULL,
   `sent_date` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_m_sender_idx` (`sender_id`),
+  KEY `fk_m_receiver_idx` (`receiver_id`),
+  CONSTRAINT `fk_m_receiver` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_m_sender` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 
 CREATE TABLE `block_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `blocker_id` int(11) NOT NULL,
   `blocked_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_blocker_user_idx` (`blocker_id`),
+  KEY `fk_blocked_user_idx` (`blocked_id`),
+  CONSTRAINT `fk_blocked_user` FOREIGN KEY (`blocked_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_blocker_user` FOREIGN KEY (`blocker_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -82,7 +98,9 @@ CREATE TABLE `volunteer_information` (
   `categories` varchar(1000) DEFAULT NULL,
   `end_time` timestamp NULL DEFAULT NULL,
   `note` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `fk_v_s_idx` (`source_website_id`),
+  CONSTRAINT `fk_v_s` FOREIGN KEY (`source_website_id`) REFERENCES `source_website` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
